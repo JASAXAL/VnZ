12cc437c095778f739d6c1e214a3e46dc81d6313
Shadow <baueebauee2@gmail.com>
2026-05-10 06:11:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 //Token pakai token bot tele lu
 
 module.exports = {
-  token: '',
-  namaBot: 'SyuzoBot V1',
-  owner: 'Yuss Xy',
-  noWa: '6283183469343'
+  token: '8601098592:AAEJHzp73fh6s29Ebx6G9faPe6txteAxudo',
+  namaBot: 'Delta',
+  owner: 'RitsuX7',
+  noWa: '6283891475720'
 }
